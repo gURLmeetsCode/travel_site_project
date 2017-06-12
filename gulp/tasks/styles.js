b05a9f3fe191ba4b43c1b6f002f2fcadb7fbfadf
Natasha <kellynatashav@yahoo.com>
2017-06-12 13:32:27 -0400

    completed various design features

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var mixins = require('postcss-mixins'); // mixins need to be placed before vars and nested in order to work.
 var cssvars = require('postcss-simple-vars');
 var autoprefixer = require('autoprefixer');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 
 
+
+
 gulp.task('styles', function(){
    return gulp.src('./app/assets/styles/styles.css')
-   .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+   .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 //    for gulp error handling
    .on('error', function(errorInfo){
        console.log(errorInfo.toString());
