8fb1d0723288b120ea5679de985c9ad24606c031
Natasha <kellynatashav@yahoo.com>
2017-06-13 16:24:13 -0400

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@
 
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 
@@ -9,4 +10,4 @@ import $ from 'jquery';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%"); // for feature items
 new RevealOnScroll($(".testimonials"), "60%"); // for testimonials section
-
+var stickyHeader = new StickyHeader();
