c45df5ac7913ab1f65df51cadd8a23473175d088
Natasha <kellynatashav@yahoo.com>
2017-06-13 18:04:36 -0400

    completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 
@@ -11,3 +12,5 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%"); // for feature items
 new RevealOnScroll($(".testimonials"), "60%"); // for testimonials section
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
+
